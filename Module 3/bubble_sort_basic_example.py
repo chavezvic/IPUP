c923f25ed8206b4b93daf37350d3a85f69f89509
Victor Chavez <59758391+chavezvic@users.noreply.github.com>
2025-11-03 16:11:26 -0600

    Create bubble_sort_basic_example.py

---
+++ Module 3/bubble_sort_basic_example.py
@@ -0,0 +1,6 @@
+arr = [64, 25, 12, 22, 11]
+for i in range(len(arr)):
+    for j in range(len(arr) - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+print("Sorted list:", arr)
