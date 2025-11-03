f4bca1c80aae20d47f8f470174820230795cdcfa
Victor Chavez <59758391+chavezvic@users.noreply.github.com>
2025-11-03 16:37:05 -0600

    Create function_multiple_return_values.py

---
+++ Module 4/function_multiple_return_values.py
@@ -0,0 +1,10 @@
+def calculate(a, b):
+    sum_result = a + b
+    product_result = a * b
+    return sum_result, product_result
+
+# Function call
+sum_result, product_result = calculate(3, 4)
+
+print("Sum:", sum_result)
+print("Product:", product_result)
