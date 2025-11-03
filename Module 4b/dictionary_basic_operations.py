5790d11f8ec50d322a855fba48923fa4e367ddfd
Victor Chavez <59758391+chavezvic@users.noreply.github.com>
2025-11-03 16:56:23 -0600

    Create dictionary_basic_operations.py

---
+++ Module 4b/dictionary_basic_operations.py
@@ -0,0 +1,9 @@
+#Dictionary
+my_dict = {"name":"Alice", "age":30, "city":"Morden"}
+print(my_dict)
+name = my_dict["name"]
+age = my_dict.get("age")
+my_dict["age"] = 31
+print(my_dict)
+my_dict["country"] = "Canada"
+print(my_dict)
