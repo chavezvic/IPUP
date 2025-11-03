02da65636396fe4d8f533ef39172e13236259e70
Victor Chavez <59758391+chavezvic@users.noreply.github.com>
2025-11-03 15:57:39 -0600

    Create if_elif_else_example.py

---
+++ Module 3/if_elif_else_example.py
@@ -0,0 +1,7 @@
+example = 7
+if example > 10:
+    print("example is greater than 10")
+elif example > 5:
+    print("example us greater than 5 but not greater than 10")
+else:
+    print("example is not greater than 5")
